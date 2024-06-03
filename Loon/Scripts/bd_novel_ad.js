1c86915d3716303b94ae81ff7355beba68edc68a
lifei6671 <longfei6671@163.com>
2024-06-03 19:38:01 +0800

    增加脚本

--- Loon/Scripts/bd_novel_ad.js
+++ Loon/Scripts/bd_novel_ad.js
@@ -1,12 +1,10 @@
 // 2024-06-03 19:00
-
-console.log("bd_novel_ad");
 const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
 console.log($response.body);
-console.log("bd_novel_ad" + url);
+console.log("bd_novel_ad:" + url);
 
 if (url.includes("/searchbox")) {
     const uri = new URL($request.url);
@@ -16,12 +14,28 @@ if (url.includes("/searchbox")) {
     console.log("请求类型:"  + params.get("type"));
     //拦截推荐
     if ( $type === "recommend") {
-        console.log(JSON.stringify(obj?.data?.novel?.recommend))
-        if (obj?.data?.length > 0 && obj?.data?.novel?.recommend) {
+        if (obj?.data?.novel?.recommend) {
             obj.data.novel.recommend = {};
         }
     } else if($type === "adword") {
         obj.data = {};
+    } else if ($type === "piratedcontentextra") {
+        if (obj?.data?.novel?.piratedcontentextra?.dataset) {
+            obj.data.novel.piratedcontentextra.dataset.popup = {};
+            obj.data.novel.piratedcontentextra.dataset.adbanner = {};
+            obj.data.novel.piratedcontentextra.dataset.adAtmosphere = {};
+            obj.data.novel.piratedcontentextra.dataset.rewardAdInfo = {};
+
+            if (obj?.data?.novel?.piratedcontentextra?.dataset?.pass_back) {
+                obj.data.novel.piratedcontentextra.dataset.pass_back.hijack_in_whitelist = 0;
+            }
+        }
+    } else if ($type === "piratedad") {
+
+    } else if($type === "piratedbaradbanner") {
+        if (obj?.data?.novel?.piratedbaradbanner?.dataset) {
+            obj.data.novel.piratedbaradbanner.dataset = {};
+        }
     }
 }
 
