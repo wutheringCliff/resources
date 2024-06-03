1fedb88924283768dd1319d86913bf26f7620caf
lifei6671 <longfei6671@163.com>
2024-06-03 18:49:01 +0800

    增加脚本

--- Loon/Scripts/bd_novel_ad.js
+++ Loon/Scripts/bd_novel_ad.js
@@ -11,13 +11,16 @@ console.log("bd_novel_ad" + url);
 if (url.includes("/searchbox")) {
     // 使用URLSearchParams解析查询字符串
     const params = new URLSearchParams(url.search);
+    const $type = params.get("type");
     console.log("请求类型:"  + params.get("type"));
     //拦截推荐
-    if (params.get("type") === "recommend") {
+    if ( $type === "recommend") {
         console.log(JSON.stringify(obj?.data?.novel?.recommend))
         if (obj?.data?.length > 0 && obj?.data?.novel?.recommend) {
             obj.data.novel.recommend = {};
         }
+    } else if($type === "adword") {
+        obj.data = {};
     }
 }
 
