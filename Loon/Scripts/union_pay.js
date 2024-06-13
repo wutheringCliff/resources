9428cf3db18cee91b4b5c86c7b3892f7a86ee236
lifei6671 <longfei6671@163.com>
2024-06-13 18:44:31 +0800

    增加脚本

--- Loon/Scripts/union_pay.js
+++ Loon/Scripts/union_pay.js
@@ -1,7 +1,7 @@
 // 2024-06-13 19:00
 const url = $request.url;
 if (!$response.body) $done({});
-let obj = JSON.parse($response.body);
+//let obj = JSON.parse($response.body);
 
 console.log("union_pay_url: " + url);
 console.log("union_pay_body: " + $response.body);
