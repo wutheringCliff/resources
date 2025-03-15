c564a8d7439ba345a8e544f85cfeb4a3bf2a060f
wutheringCliff <11626254+wutheringcliff@users.noreply.github.com>
2025-03-15 16:17:00 +0800

    debug4

--- QuantumultX/Scripts/Finka.js
+++ QuantumultX/Scripts/Finka.js
@@ -6,31 +6,31 @@ let obj = JSON.parse($response.body);
 console.log("Finka_Request_URL: " + url);
 console.log("Finka_Response_Body: " + $response.body);
 
-if (url.includes("/feed/v5")) {
-    if (obj.hasOwnProperty("data")) {
-        if (obj.data.hasOwnProperty("list")) {
-            obj.data.list = obj.data.list.filter((feed) => feed.hasOwnProperty("type") && feed.type !== "ShowCase")
-        }
-    }
-}
-if (url.includes("/user/profile/view/v3")) {
-    if (obj.data?.hasOwnProperty("user")) {
-        const user = obj.data.user;
-        if (user.hasOwnProperty("vip")) user.vip = true;
-        if (user.hasOwnProperty("ssvip")) user.ssvip = true;
-        if (user.hasOwnProperty("svip")) user.svip = true;
-    }
-}
+// if (url.includes("/feed/v5")) {
+//     if (obj.hasOwnProperty("data")) {
+//         if (obj.data.hasOwnProperty("list")) {
+//             obj.data.list = obj.data.list.filter((feed) => feed.hasOwnProperty("type") && feed.type !== "ShowCase")
+//         }
+//     }
+// }
+// if (url.includes("/user/profile/view/v3")) {
+//     if (obj.data?.hasOwnProperty("user")) {
+//         const user = obj.data.user;
+//         if (user.hasOwnProperty("vip")) user.vip = true;
+//         if (user.hasOwnProperty("ssvip")) user.ssvip = true;
+//         if (user.hasOwnProperty("svip")) user.svip = true;
+//     }
+// }
+//
+// if (url.includes("/user/profile/info/v2")) {
+//     let data = obj.data;
+//     if (data) {
+//         if (data.hasOwnProperty("ad")) data.ad = [];
+//         if (data.hasOwnProperty("profilePasterAd")) data.profilePasterAd = {};
+//         if (data.hasOwnProperty("featuredServicesList")) data.featuredServicesList = [];
+//     }
+// }
 
-if (url.includes("/user/profile/info/v2")) {
-    let data = obj.data;
-    if (data) {
-        if (data.hasOwnProperty("ad")) data.ad = [];
-        if (data.hasOwnProperty("profilePasterAd")) data.profilePasterAd = {};
-        if (data.hasOwnProperty("featuredServicesList")) data.featuredServicesList = [];
-    }
-}
-
-console.log("Finka_Response_Revised_Body: " + JSON.stringify(obj));
+// console.log("Finka_Response_Revised_Body: " + JSON.stringify(obj));
 
 $done({body: JSON.stringify(obj)});
