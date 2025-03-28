9c03b1d309b02fdff48525471e9e1ed05c00945b
wutheringCliff <111626254+wutheringCliff@users.noreply.github.com>
2025-03-28 16:26:50 +0800

    debug

--- QuantumultX/Scripts/blued.js
+++ QuantumultX/Scripts/blued.js
@@ -9,6 +9,8 @@ try {
     $done({});
 }
 
+const replaceFirstNumber = (str, num) => str.replace(/(\D*?)(\d+)(.*)/, `$1${num}$3`);
+
 const RULES = [
     {
         pattern: /^https:\/\/social\.blued\.cn\/users\/\d+\/visitors\?/,
@@ -45,6 +47,13 @@ const RULES = [
                     if (typeof item?.free_times === 'number') {
                         item.free_times = 10;
                     }
+                    if (item.flash_prompt){
+                        item.flash_prompt = replaceFirstNumber(item.flash_prompt, 10);
+                    }
+                    if (item.notice_text){
+                        item.notice_text = replaceFirstNumber(item.notice_text, 10);
+                    }
+
                     if (item?.is_vip === 0) item.is_vip = 1
 
                     result.push(item);
@@ -58,9 +67,14 @@ const RULES = [
 ];
 
 RULES.some(({pattern, handler}) => {
-    if (pattern.test(url)) {
+    console.log(`pattern(${pattern})`);
+    let flag = pattern.test(url);
+    console.log(`isMatched:${flag}`)
+    if (flag) {
         handler(obj);
         return true;
+    } else {
+        console.log(`Non-matched Blued_Request_URL: ${url}`);
     }
     return false;
 });
